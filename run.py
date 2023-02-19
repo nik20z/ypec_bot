a8ff82127e28a7df716d3c440334877e7eeb8541
Nikita <62090150+nik20z@users.noreply.github.com>
2023-02-19 20:52:37 +0300

    Add files via upload

--- run.py
+++ run.py
@@ -1,15 +1,16 @@
 import asyncio
 import nest_asyncio
 from loguru import logger
+
 from bot.tg_module import start_telegram_bot
 from bot.vk_module import start_vk_bot
-
+from bot.database import Table
 from bot.spamming.main import check_replacement
 from bot.spamming.functions import get_next_check_time
 from bot.config import array_times
 
 
-def repeat(func, loop_repeat):
+def repeat(func, loop_repeat) -> None:
     """Функция-повторитель для loop"""
     asyncio.ensure_future(func(), loop=loop_repeat)
     interval = get_next_check_time(array_times, func.__name__)
@@ -33,6 +34,10 @@ if __name__ == '__main__':
             loop.create_task(start_vk_bot())
         ]
 
-        loop.call_later(5, repeat, check_replacement, loop)
+        Table.create_extensions()
+        Table.create()
+        Table.create_view()
+
+        CHECK_REPLACEMENT_LOOP = loop.call_later(3, repeat, check_replacement, loop)
 
         loop.run_until_complete(asyncio.wait(tasks))
